f1c218d9b67c220fb7261e6093a7e70676f67f37
Gayathriprabha Mallavarapu <gayathriprabha_mallavarapu@epam.com>
2022-02-15 19:02:44 +0530

    Commit

--- selenium/src/test/java/TestAutomation/selenium/TestWebDriver.java
+++ selenium/src/test/java/TestAutomation/selenium/TestWebDriver.java
@@ -15,22 +15,9 @@ public class TestWebDriver {
 		System.setProperty("webdriver.gecko.driver", driverPath);
 		WebDriver driver=new FirefoxDriver();
 		driver.get("https://www.orangehrm.com/");
-		driver.manage().window().maximize();
-		/*String driverPathChrome="C:\\browserDriver\\chromedriver_win32\\chromedriver.exe";
+		String driverPathChrome="C:\\browserDriver\\chromedriver_win32\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPathChrome);
 		WebDriver chromeDriver=new ChromeDriver();
-		chromeDriver.get("https://www.orangehrm.com/");*/
-		driver.findElement(By.id("myText")).sendKeys("abc@gmail.com");
-		driver.findElement(By.id("linkadd")).click();
-		driver.findElement(By.id("Form_submitForm_subdomain")).sendKeys("https:demo.com");
-		driver.findElement(By.id("Form_submitForm_Name")).sendKeys("xyz");
-		driver.findElement(By.id("Form_submitForm_Contact")).sendKeys("6392456178");
-		WebElement dropdown=driver.findElement(By.id("Form_submitForm_Country"));
-		Select drop=new Select(dropdown);
-		drop.selectByVisibleText("India");
-		WebElement dropdowns=driver.findElement(By.id("Form_submitForm_State"));
-		Select drops=new Select(dropdowns);
-		drops.selectByVisibleText("Telangana");
-		driver.findElement(By.id("Form_submitForm_action_request")).click();
+		chromeDriver.get("https://www.orangehrm.com/");
 	}
 }
